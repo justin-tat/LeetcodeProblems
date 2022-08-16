91dca9ccb3fba172a5dd760ffb1a176160f59005
Justin <jtat5912@gmail.com>
2022-08-16 16:20:28 -0700

    Implemented power of function

---
+++ medium/Pow(x, n)/pow(x,n).js
@@ -0,0 +1,22 @@
+/**
+ * @param {number} x
+ * @param {number} n
+ * @return {number}
+ */
+ var myPow = function(x, n) {
+  if(n == 0 || x == 1.0){
+      return 1;
+  }
+if(n == 1){
+      return x;
+  }
+else if(n % 2 == 0) {
+      return myPow(x * x, n / 2);
+  }
+else if(n % 2 == 1){
+      return x * myPow(x, n - 1);
+  }
+else{
+      return 1/myPow(x, -n);
+  }
+};
